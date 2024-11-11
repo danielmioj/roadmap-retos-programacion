ee5d3784060dd015b38a2eccc559a64caf4eb6da
danielmioj <84147536+danielmioj@users.noreply.github.com>
2024-11-10 19:02:09 -0500

    danielmioj.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/danielmioj.py
@@ -0,0 +1,32 @@
+# Comentario de una línea
+
+"""
+Esto es un comentario de
+varias líneas
+"""
+
+
+'''
+Esto es un comentario
+en varias líneas
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" # por convención
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string = 'Otra cadena de texto'
+
+print("¡Hola, Pyhon!")
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
+
+print(my_int)
